6ddf745b9eba30b80c8ee4a6fcd274fcbd55317d
Ricardo <rcsilva2405@gmail.com>
2022-11-16 08:37:35 -0500

    hfafad

--- app/views/page-login_cliente.php
+++ app/views/page-login_cliente.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?=URL?>/assets/css/style-login.css">
+    <link rel="shortcut icon" href="../../assets/img/grape.png" type="image/x-icon">
     <title><?=$params['informacoes_da_pagina']['titulo']?></title>
 </head>
 <body>
